b91ed68e3157d92ac8173a2065861c5b40351fbd
Burmsey <joehurndell1@gmail.com>
2025-05-06 11:42:59 +1200

    thingy mamoo thinga jajoo. (made it clear options, working on showing cart)

--- store.py
+++ store.py
@@ -1,9 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
 
-class Menu:
-    def __init__(self):
+class Cart:
+    def __init__(self, drinks, mains, sides):
+        self.drinks = drinks
+        self.mains = mains
+        self.sides = sides
         pass
+        #store name, address, phone number in here?
 
 class Store:
     def __init__(self, root):
@@ -11,6 +15,8 @@ class Store:
         self.root.title("Burger Joint")
         self.home_screen = Frame(root)
         self.delivery_frame = Frame(root)
+
+        self.items_in_cart = []
 
         self.home_screen.grid(row=0, column=0, padx=100, pady=100, sticky="ew")
 
@@ -37,34 +43,83 @@ class Store:
 
         self.text = Label(self.delivery_frame, text="Menu Options")
 
-        self.drink_var = StringVar() 
+        self.drink_choice = StringVar() 
+        self.main_choice = StringVar()
+        self.sides_choice = StringVar()
         self.drinks_label = Label(self.delivery_frame, text="Drinks")
-        self.drinks_dropdown = OptionMenu(self.delivery_frame, self.drink_var, "Coke", "Sprite")
+        self.drinks_dropdown = OptionMenu(self.delivery_frame, self.drink_choice, "Coke", "Sprite")
         self.main_label = Label(self.delivery_frame, text="Main")
-        self.main_dropdown = OptionMenu(self.delivery_frame, StringVar(), "Pizza", "Burger", "Salad")
+        self.main_dropdown = OptionMenu(self.delivery_frame, self.main_choice, "Pizza", "Burger", "Salad")
         self.sides_label = Label(self.delivery_frame, text="Sides")
-        self.sides_dropdown = OptionMenu(self.delivery_frame, StringVar(), "Fries")
+        self.sides_dropdown = OptionMenu(self.delivery_frame, self.sides_choice, "Fries")
 
-        self.show_btn = Button(self.delivery_frame, text="Show Drink", command=self.show)
-        self.show_btn.grid(row=3, column=0, padx=5, pady=5)
+        self.show_btn = Button(self.delivery_frame, text="Add To Cart", command=self.added_to_cart)
+        self.cart = Button(self.delivery_frame, text="Show Cart", command=self.show_cart)
+        self.show_btn.grid(row=4, columnspan=6, padx=5, pady=5)
+        self.cart.grid(row=5, columnspan=6, padx=5, pady=5)
 
-        self.lbl = Label(self.delivery_frame, text="")
-
-        self.text.grid(row=0, column=1, padx=10, pady=10)
+        self.drink_choice_clear = Button(self.delivery_frame, text="Clear", command=self.drinks_clear)
+        self.main_choice_clear = Button(self.delivery_frame, text="Clear", command=self.main_clear)
+        self.sides_choice_clear = Button(self.delivery_frame, text="Clear", command=self.sides_clear)
 
         self.drinks_label.grid(row=1, column=0, padx=5, pady=5)
         self.drinks_dropdown.grid(row=2, column=0, padx=5, pady=5)
+        self.drink_choice_clear.grid(row=3, column=0, padx=5, pady=5)
 
         self.main_label.grid(row=1, column=1, padx=5, pady=5)
         self.main_dropdown.grid(row=2, column=1, padx=5, pady=5)
+        self.main_choice_clear.grid(row=3, column=1, padx=5, pady=5)
 
         self.sides_label.grid(row=1, column=2, padx=5, pady=5)
         self.sides_dropdown.grid(row=2, column=2, padx=5, pady=5)   
+        self.sides_choice_clear.grid(row=3, column=2, padx=5, pady=5)
 
-    def show(self):
-        selected_drink = self.drink_var.get()
-        self.lbl.config(text=f"Selected Drink: {selected_drink}")
-        print(selected_drink)
+    def added_to_cart(self):
+        self.cart = []
+        drink = self.drink_choice.get()
+        main = self.main_choice.get()
+        sides = self.sides_choice.get()
+
+        self.items_in_cart.append(Cart(drink, main, sides))
+
+        self.drink_choice.set("")
+        self.main_choice.set("")
+        self.sides_choice.set("")
+    
+    def show_cart(self):
+        coke = 0
+        sprite = 0
+        pizza = 0
+        burger = 0
+        salad = 0
+        fries = 0
+
+        for i in self.items_in_cart:
+            if i == "Coke":
+                coke += 1
+            elif i == "Sprite":
+                sprite += 1
+            elif i == "Pizza":
+                pizza += 1
+            elif i == "Burger":
+                burger += 1
+            elif i == "Salad":
+                salad += 1
+            elif i == "Fries":
+                fries += 1
+        
+        print(f"Coke: {coke}x, Sprite: {sprite}x, Pizza: {pizza}x, Burger: {burger}x, Salad: {salad}x, Fries: {fries}x")
+        print(self.items_in_cart)
+    
+    def drinks_clear(self):
+        self.drink_choice.set("")
+    
+    def main_clear(self):
+        self.main_choice.set("")
+    
+    def sides_clear(self):
+        self.sides_choice.set("")
+
             
             
         
